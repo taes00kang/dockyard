b0bf4fc7ed1c350406bbcba35be32b6d0df6eb38
Taesoo <75271289+taesookang@users.noreply.github.com>
2023-02-06 01:24:08 -0500

    FIX: title & favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,8 +38,25 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>TDS</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>The Dockyard Social</title>
+        <link rel="shortcut icon" href="/favicon/favicon.ico?" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
       </Head>
       <Header />
       <Section1 />
